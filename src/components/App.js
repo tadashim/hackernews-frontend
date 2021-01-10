2d496da0f5b2ac85750a3de45eb0932f6a555a6b
tadashim <ma.tadashi@gmail.com>
2021-01-10 22:12:51 +0900

    add search feature

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import CreateLink from './CreateLink';
 import Header from './Header';
 import LinkList from './LinkList';
 import Login from './Login';
+import Search from './Search';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
           <Route exact path="/" component={LinkList} />
           <Route exact path="/create" component={CreateLink} />
           <Route exact path="/login" component={Login} />
+          <Route exact path="/Search" component={Search} />
         </Switch>
       </div>
     </div>
